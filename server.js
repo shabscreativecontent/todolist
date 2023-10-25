5b412be6902bb1ea76dffc623ed0a2095240b317
ShabsCreativeContent <ajideshedrack@gmail.com>
2023-10-26 00:33:40 +0100

    todoList creation

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const connectDB = require('./config/database')
+const authRoutes = require('./routes/auth')
+const homeRoutes = require('./routes/home')
+const todoRoutes = require('./routes/todos')
+
+
+ 
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on ${process.env.PORT}, you better catch it!`)
+})    
